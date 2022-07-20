bbf193133d959eb0a79a7d2ded5ae546285465d0
ABelikhin <63578650+ABelikhin@users.noreply.github.com>
2022-07-20 11:20:06 +0300

    Update server.c

--- server.c
+++ server.c
@@ -5,49 +5,63 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
-int main(int argc, char **argv){
+#define BUFF_SIZE 1500
 
+int main(int argc, char **argv)
+{
   if (argc != 2){
     printf("Use command: %s <port>\n", argv[0]);
     exit(0);
   }
 
-  char *ip = "127.0.0.1";
-  int port = atoi(argv[1]);
+  int port = atoi(argv[1]); // лучше использовать strtol
 
-  int sockfd;
-  struct sockaddr_in server_addr, client_addr;
-  char buffer[1024];
-  socklen_t addr_size;
-  int n;
-
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  int sockfd = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
   if (sockfd < 0){
     perror("[-]socket error");
     exit(1);
   }
 
-  memset(&server_addr, '\0', sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port);
-  server_addr.sin_addr.s_addr = inet_addr(ip);
+  struct sockaddr_in server_addr = {
+    .sin_family = AF_INET,
+    .sin_port = htons(port),
+    .sin_addr = { .s_addr = INADDR_ANY }
+  };
 
-  n = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (n < 0) {
+  int rc = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if (rc < 0) {
     perror("[-]bind error");
     exit(1);
   }
 
-  bzero(buffer, 1024);
-  addr_size = sizeof(client_addr);
-  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
-  printf("[+]Data recv: %s\n", buffer);
+  while(1) {
+    char buffer[BUFF_SIZE]; // = {0}
+    struct sockaddr_in client_addr;
+    socklen_t addr_size = sizeof(client_addr);
 
-  bzero(buffer, 1024);
-  strcpy(buffer, "Server online");
-  sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
-  printf("[+]Data send: %s\n", buffer);
+    select(); //
+    ssize_t read_len = recvfrom(sockfd, buffer, BUFF_SIZE, 0, (struct sockaddr*)&client_addr, &addr_size);
+    if (read_len < 0) {
+      if (errno == EINTR) {
+        continue;
+      }
+      perror("[-]recvfrom error");
+      exit(1);
+    }
+    buffer[read_len] = '\0';
+    printf("[+]Data recv: %s\n", buffer);
 
+    const char *reply = "Server online\n";
+    strcpy(buffer, reply);
+    ssize_t sent_len = sendto(sockfd, buffer, strlen(reply), 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    if (sent_len < 0) {
+      if (errno == EINTR) {
+        // ???
+      }
+    }
+    printf("[+]Data send: %s\n", buffer);
+  }
   return 0;
 }
