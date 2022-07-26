a7181b8ce22d43ebd1e0b20fa2bf8ef357b26a39
ABelikhin <63578650+ABelikhin@users.noreply.github.com>
2022-07-26 09:38:55 +0300

    Update server.c

--- server.c
+++ server.c
@@ -7,108 +7,84 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <sys/select.h>
+//#include <io.h>
 
-#define BUFF_SIZE 1500
+#define BUFFLEN 1500
 
-int main(int argc, char **argv)
-{
- 
+
+
+int main(int argc, char **argv){
   if (argc != 2){
-    printf("Use command: %s <port>\n", argv[0]);// proverka commandi
+    printf("Use command: %s <port>\n", argv[0]);
     exit(0);
   }
-
-  int port = atoi (argv[1]); // лучше использовать strtol
-  ssize_t read_len;
-  int sockfd = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);// socket sozdan
-  if (sockfd < 0){
-    perror("[-]socket error");
-    exit(1);
-  }
-
-  struct sockaddr_in server_addr = {//addr_socketa
+  int port = atoi (argv[1]); 
+  
+  struct sockaddr_in server = {
     .sin_family = AF_INET,
     .sin_port = htons(port),
     .sin_addr = { .s_addr = INADDR_ANY }
   };
-
-  int rc = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (rc < 0) {
-    perror("[-]bind error");//socket s addresom
+  int sock = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
+  if (sock < 0){
+    perror("[-]socket error");
+    exit(1);
+  }
+  int err = bind(sock, (struct sockaddr*)&server, sizeof(server));
+  if (err < 0) {
+    perror("[-]bind error");
     exit(1);
   }
 
   fd_set active_set, read_set;
   FD_ZERO(&active_set);
-  FD_SET(sockfd,&active_set);
+  FD_SET(sock,&active_set);
   int i;
-  socklen_t size;
-};
-  
-  while (1) {
+  int new_sock;
 
+  while(1){
     
-
-    char buffer[BUFF_SIZE]; // = {0}
-    struct sockaddr_in client_addr;
-    socklen_t addr_size = sizeof(client_addr);
-
-    //select()
-    int i;
-    socklen_t size;
-    struct client;
-
+    char buff[BUFFLEN];
+    struct sockaddr_in client;
+    socklen_t size = sizeof(client);
     read_set=active_set;
-    if(select(FD_SETSIZE,&read_set,NULL,NULL,NULL)<0){//SELECT WORK
+    if( select(FD_SETSIZE,&read_set,NULL,NULL,NULL)<0){
       perror("[-]select error");
       exit(EXIT_FAILURE);
-    }
-    for(i=0;i<FD_SETSIZE;i++){
+    };
+    for (i=0;i<FD_SETSIZE;i++){
       if(FD_ISSET(i,&read_set)){
-        if(i==sockfd){
-          size=sizeof(client_addr);
-          int new_sock = accept(sockfd,(struct sockaddr*)&client_addr,&addr_size);
+        if(i==sock){
+          size = sizeof(client);
+          new_sock=accept(sock,(struct sockaddr*)&client,&size);
           if(new_sock<0){
-            perror("[-]accept err");
+            perror("accept");
             exit(EXIT_FAILURE);
           }
-          printf("[+]Data recv: %s\n", buffer);
-          FD_SET(new_sock,&active_set);
-        } else { 
-            int err = read_len;
-            if(err<0){
-              close(i);
-              FD_CLR(i,active_set);
-            } else{ 
-              if(strstr(buffer,"stop")){
-                close(i);
-                FD_CLR(i,active_set);
-              }
-        } else {
-      }
-    }
-  
-    read_len = recvfrom(sockfd, buffer, BUFF_SIZE, 0, (struct sockaddr*)&client_addr, &addr_size);
-    if (read_len < 0) {
-      if (errno == EINTR) {
-        continue;
-      }
-      perror("[-]recvfrom error");
-      exit(1);
-    } // chtenie
-    buffer[read_len] = '\0';
-    printf("[+]Data recv: %s\n", buffer);
+        }
+          fprintf(stdout,"server connect from host %s, port %hu.\n",
+            inet_ntoa(client.sin_addr),
+            ntohs(client.sin_port));
+            FD_SET(new_sock,&active_set);
+      }else {
+        err = recvfrom(sock, buff, BUFFLEN, 0, (struct sockaddr*)&client, &size);
+        if (err<0) {
+          close(i);
+          FD_CLR(i,&active_set);
 
-    const char *reply = "Server online\n";
-    strcpy(buffer, reply);
-    ssize_t sent_len = sendto(sockfd, buffer, strlen(reply), 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
-    if (sent_len < 0) {
-      if (errno == EINTR) {
-        // ???
+        }else{
+          if(strstr(buff,"stop")){
+            close(i);
+            FD_CLR(i,&active_set);
+          }else{
+            const char *reply = "Server msg\n";
+            strcpy(buff, reply);
+            ssize_t sent_len = sendto(sock, buff, strlen(reply), 0, (struct sockaddr*)&client, sizeof(client));
+            printf("Data send: %s\n", buff);
+          }
+        }
       }
     }
-    printf("[+]Data send: %s\n", buffer);// otpravka
   }
-  } 
-  return 0;
 }
+
